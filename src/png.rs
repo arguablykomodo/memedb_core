8d5d8e704f783ce3aff757d42e9feadc5d636e26
Sr Komodo <komodosr@gmail.com>
2019-06-27 23:06:38 -0300

    implement png tag reading

--- src/png.rs
+++ src/png.rs
@@ -1,17 +1,49 @@
 use crate::tags::Tags;
 
-static SIGNATURE: &[u8] = &[0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
+const SIGNATURE: &[u8] = &[0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
 
 pub fn read_tags(bytes: &[u8]) -> Option<Tags> {
-  let mut bytes = bytes.iter();
-
+  let mut i = 0;
   for signature_byte in SIGNATURE {
-    if bytes.next()? != signature_byte {
+    if bytes[i] != *signature_byte {
       return None;
     }
+    i += 1;
   }
 
-  Some(Tags::new())
+  loop {
+    let mut length = 0;
+    for j in 0..4 {
+      length += (bytes[i] as usize) << (3 - j) * 8;
+      i += 1;
+    }
+
+    let chunk_type = &bytes[i..(i + 4)];
+    i += 4;
+
+    if chunk_type == b"meMe" {
+      let data = &bytes[i..(i + length)];
+      let mut tags = Tags::new();
+      let mut text = String::new();
+      for byte in data {
+        if *byte == b';' {
+          tags.add_tag(text);
+          text = String::new();
+        } else {
+          text.push(*byte as char);
+        }
+      }
+      return Some(tags);
+    }
+
+    // All PNG files must end with an IEND chunk
+    if chunk_type == b"IEND" {
+      return Some(Tags::new());
+    }
+
+    // Every chunk ends with a 4 byte long checksum
+    i += length + 4;
+  }
 }
 
 #[cfg(test)]
@@ -21,9 +53,12 @@ mod tests {
   #[test]
   fn test_read() {
     assert_eq!(read_tags(include_bytes!("../tests/jpg.jpg")), None);
-    assert_eq!(
-      read_tags(include_bytes!("../tests/png.png")),
-      Some(Tags::new())
-    );
+
+    let tags = Tags::new();
+    assert_eq!(read_tags(include_bytes!("../tests/empty.png")), Some(tags));
+
+    let mut tags = Tags::new();
+    tags.add_tag(String::from("test"));
+    assert_eq!(read_tags(include_bytes!("../tests/tagged.png")), Some(tags));
   }
 }
