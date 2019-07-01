1a868f7b1fb3bdc30e24645aee292b12606fac0e
SrKomodo <komodosr@gmail.com>
2019-07-01 14:11:01 -0300

    add write_tags method

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
+pub mod error;
 mod png;
 mod reader;
-pub mod error;
 
 use error::Error;
 use reader::Reader;
-use std::fs::File;
-use std::io::Read;
 use std::collections::HashSet;
+use std::fs::{File, OpenOptions};
+use std::io::Read;
 
 pub fn read_tags(path: String) -> Result<HashSet<String>, Error> {
   let mut bytes = File::open(path)?.bytes();
   png::PngReader::read_tags(&mut bytes)
+}
+
+pub fn write_tags(path: String, tags: HashSet<String>) -> Result<(), Error> {
+  let mut file = OpenOptions::new()
+    .read(true)
+    .write(true)
+    .open(path)
+    .unwrap();
+  png::PngReader::write_tags(&mut file, &tags)?;
+  Ok(())
 }
