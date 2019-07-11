635b5a1d9c7a76ced1d98195918e63373796b10b
Sr Komodo <komodosr@gmail.com>
2019-07-10 23:18:46 -0300

    get color table info

--- src/gif.rs
+++ src/gif.rs
@@ -17,6 +17,12 @@ impl Reader for GifReader {
         }
         i += 6;
 
+        // Get info we need from Logical Screen Descriptor
+        let has_color_table = bytes[10] >> 7 == 1;
+        let color_table_size =
+            2 << (bytes[10] & 1 + bytes[10] >> 1 & 1 * 2 + bytes[10] >> 2 & 1 * 4 + 1);
+        i += 7;
+
         unimplemented!();
     }
     fn write_tags(file: &mut impl Read, tags: &HashSet<String>) -> Result<Vec<u8>, Error> {
