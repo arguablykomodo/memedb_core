27d8f826df649854d5e8d1a127b3aa1da957bc27
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2019-07-26 23:48:09 -0300

    Improved error handling and added guards against corrupted files

--- src/jpg.rs
+++ src/jpg.rs
@@ -3,7 +3,8 @@ use crate::reader::Reader;
 use crate::xml::{XmlTag, XmlTree};
 use colored::*;
 use std::collections::HashSet;
-use std::io::{Bytes, Read};
+use std::io::Read;
+use std::iter::Peekable;
 use std::time::SystemTime;
 
 const SIGNATURE: &[u8] = &[0xFF, 0xD8];
@@ -44,7 +45,7 @@ impl Reader for JpgReader {
   fn read_tags(file: &mut impl Read) -> Result<HashSet<String>, Error> {
     let started = SystemTime::now();
     let mut tags: HashSet<String> = HashSet::new();
-    let mut file_iterator: std::iter::Peekable<_> = file
+    let mut file_iterator: Peekable<_> = file
       .bytes()
       /* .enumerate()
       .map(|(a, v)| {
@@ -52,43 +53,40 @@ impl Reader for JpgReader {
         v
       }) */
       .peekable();
-
     for byte in SIGNATURE.iter() {
       if *byte != read!(file_iterator)? {
         return Err(Error::UnknownFormat);
       }
     }
-    let mut chunk_header: [u8; 2];
+    let mut chunk_type: u8;
     let mut chunk_length: usize;
     loop {
-      let file_iterator_ref = &mut file_iterator;
-      let peeked: Option<u8> = read!(file_iterator_ref;peek);
-      if peeked == Some(0xFF) {
-        chunk_header = read!(file_iterator_ref; 2);
+      // Loops in rust have a bug where they consume variables in spite of borrowing them
+      // making them unusable in the next iteration (thus failing even to compile)
+      // Just declaring a dumb var and pointing it to the desired variable makes it usable in all the iterations
+      let mut file_iterator_ref = &mut file_iterator;
+      let peeked = match read!(file_iterator_ref) {
+        Ok(v) => v,
+        Err(_) => break,
+      };
+      if peeked == 0xFF {
+        chunk_type = read!(file_iterator_ref)?;
         //println!("Chunk header: {:#02X?}", chunk_header);
-        if chunk_header[1] == 0xD9 {
-          println!("{}", "EOF".green());
+        if chunk_type == 0x00 {
+          //eprintln!("{}", "Skipping 0xFF inside chunk data".yellow());
+          continue;
+        } else if chunk_type == 0xD9 {
+          //println!("{}", "EOF".green());
           break;
         }
-        let peeked: Option<u8> = read!(file_iterator_ref;peek);
-        if peeked == Some(0xFF) {
+        if read!(file_iterator_ref;peek) == Some(0xFF) {
           //println!("Peeked the start of another chunk");
           continue;
-        } else {
-          //println!("Peeked the data");
         }
-
-        chunk_length = 0x0000;
-        chunk_length |= (read!(file_iterator_ref)? as usize) << 8;
-        chunk_length |= read!(file_iterator_ref)? as usize;
-        chunk_length -= 2;
-
+        chunk_length = JpgReader::get_chunk_length(&mut file_iterator_ref)?;
         //println!("Chunk length: {:#06X?}", chunk_length);
         if read!(file_iterator_ref;peek) == Some(0x68) {
-          let chunk_data: Vec<u8> = file_iterator_ref
-            .take(chunk_length)
-            .map(|v| v.unwrap())
-            .collect();
+          let chunk_data = JpgReader::get_chunk_data(&mut file_iterator_ref, chunk_length)?;
           let chunk_string = String::from_utf8(chunk_data)
             .ok()
             .unwrap_or(":(".to_string());
@@ -98,6 +96,7 @@ impl Reader for JpgReader {
             Err(e) => eprintln!("Xml parser error {}", format!("{:?}", e).red()),
           }
         } else {
+          //println!("Skipping {} bytes", chunk_length);
           for _ in 0..chunk_length {
             read!(file_iterator_ref)?;
           }
@@ -116,6 +115,41 @@ impl Reader for JpgReader {
   }
 }
 impl JpgReader {
+  fn get_chunk_length(
+    file_iterator: &mut Peekable<impl Iterator<Item = Result<u8, std::io::Error>>>,
+  ) -> Result<usize, Error> {
+    let mut chunk_length = 0x0000;
+    chunk_length |= (read!(file_iterator)? as usize) << 8;
+    chunk_length |= read!(file_iterator)? as usize;
+    chunk_length -= 2;
+    return Ok(chunk_length);
+  }
+  fn get_chunk_data(
+    file_iterator: &mut Peekable<impl Iterator<Item = Result<u8, std::io::Error>>>,
+    chunk_length: usize,
+  ) -> Result<Vec<u8>, Error> {
+    let chunk_data: Vec<u8> = file_iterator
+      .take(chunk_length)
+      .map(|v| v.unwrap())
+      .collect();
+    if chunk_data.len() != chunk_length {
+      eprintln!(
+        "{}",
+        format!(
+          "Error: The data captured is shorter than expected\n{} bytes expected, got {}",
+          chunk_length,
+          chunk_data.len()
+        )
+        .red()
+      );
+      return match read!(file_iterator) {
+        Ok(v) => Err(Error::ParserError),
+        Err(e) => Err(e),
+      };
+    } else {
+      return Ok(chunk_data);
+    }
+  }
   fn parse_xml(xml: &str) -> Result<HashSet<String>, Error> {
     let tree = XmlTree::parse(xml.to_string())?;
     let finds = tree.find_elements(|e: &XmlTag| match e.attributes.get("rdf:about") {
