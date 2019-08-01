adae39049b347ba839286aee6e0dda3f4d1bd110
Sr Komodo <komodosr@gmail.com>
2019-08-01 01:27:04 -0300

    add gif to main lib

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,21 @@ use std::io::Write;
 pub type TagSet = HashSet<String>;
 
 pub fn read_tags(path: String) -> Result<TagSet, Error> {
-    let mut file = File::open(path)?;
-    png::PngReader::read_tags(&mut file)
+    let mut file = File::open(&path)?;
+    match path.split(".").last().unwrap() {
+        "png" => png::PngReader::read_tags(&mut file),
+        "gif" => gif::GifReader::read_tags(&mut file),
+        _ => Err(Error::UnknownFormat),
+    }
 }
 
 pub fn write_tags(path: &String, tags: &TagSet) -> Result<(), Error> {
     let mut file = File::open(&path)?;
-    let bytes = png::PngReader::write_tags(&mut file, &tags)?;
+    let bytes = match path.split(".").last().unwrap() {
+        "png" => png::PngReader::write_tags(&mut file, &tags)?,
+        "gif" => gif::GifReader::write_tags(&mut file, &tags)?,
+        _ => return Err(Error::UnknownFormat),
+    };
     let mut file = OpenOptions::new().write(true).truncate(true).open(&path)?;
     file.write_all(&bytes)?;
     Ok(())
