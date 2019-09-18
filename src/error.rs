b514bed96558ec7432a735a96d9802010be6ae99
Sr Komodo <komodosr@gmail.com>
2019-09-18 20:10:58 -0300

    formatting :)

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,6 @@
 use custom_error::custom_error;
 
-custom_error!{pub Error
+custom_error! { pub Error
     Format = "Invalid or unknown format",
     EOF = "Reached unexpected end of file",
     Io {source: std::io::Error} = "Error reading file"
