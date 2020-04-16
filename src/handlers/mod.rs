0e77f4466adb1da60f428d3ab6ef44975439327b
Sr Komodo <komodosr@gmail.com>
2020-04-16 14:06:48 -0300

    tweaks to identifier

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,5 +1,7 @@
 mod png;
 
+use std::io::{Bytes, Read, Result};
+
 #[derive(Copy, Clone, Debug, PartialEq)]
 enum Handler {
     Png,
@@ -7,11 +9,14 @@ enum Handler {
 
 const HANDLERS: &[(&[u8], Handler)] = &[(png::SIGNATURE, Handler::Png)];
 
-fn identify_format(bytes: std::io::Bytes<impl std::io::Read>) -> std::io::Result<Option<Handler>> {
+// Identifies the format for a file by succesively eliminating non-matching signatures until 1 remains.
+fn identify_format(bytes: &mut Bytes<impl Read>) -> Result<Option<Handler>> {
     let mut handlers = HANDLERS.to_vec();
+    // Get length of longest signature, so we know when to stop iterating
     let length = HANDLERS.iter().map(|(s, _)| s.len()).max().expect("no handlers found");
     for (i, byte) in bytes.take(length).enumerate() {
         let byte = byte?;
+        // Filter non-matching signatures
         handlers = handlers.into_iter().filter(|(s, _)| s[i] == byte).collect();
         match handlers.len() {
             1 => return Ok(Some(handlers[0].1)),
@@ -19,36 +24,40 @@ fn identify_format(bytes: std::io::Bytes<impl std::io::Read>) -> std::io::Result
             _ => (),
         }
     }
-    Ok(None)
+    // If we get here either the file is empty or something went *very* wrong
+    if bytes.next().is_none() {
+        Ok(None)
+    } else {
+        unreachable!()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::Read;
 
     #[test]
     fn correctly_identify_handlers() {
         for handler in HANDLERS {
-            assert_eq!(identify_format(handler.0.bytes()).unwrap(), Some(handler.1));
+            assert_eq!(identify_format(&mut handler.0.bytes()).unwrap(), Some(handler.1));
         }
     }
 
     #[test]
     fn unknown_format() {
         let bytes = &[0x2E, 0x7C, 0x2E, 0x2E, 0x0A, 0x2E, 0x2E, 0x7C, 0x2E, 0x2C];
-        assert_eq!(identify_format(bytes.bytes()).unwrap(), None);
+        assert_eq!(identify_format(&mut bytes.bytes()).unwrap(), None);
     }
 
     #[test]
     fn short_file() {
         let bytes = &[0x00];
-        assert_eq!(identify_format(bytes.bytes()).unwrap(), None);
+        assert_eq!(identify_format(&mut bytes.bytes()).unwrap(), None);
     }
 
     #[test]
     fn empty_file() {
         let bytes = &[];
-        assert_eq!(identify_format(bytes.bytes()).unwrap(), None);
+        assert_eq!(identify_format(&mut bytes.bytes()).unwrap(), None);
     }
 }
