6503aee97c00a76811bf2d126a62534a77b106f9
Komodo <2420624-SrKomodo@users.noreply.gitlab.com>
2021-02-21 14:25:03 -0300

    improvement: better single byte reading

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,9 @@
 macro_rules! read_bytes {
     // Return u8 if reading one byte
     ($src:expr, 1) => {{
-        let mut bytes = [0; 1];
-        $src.read_exact(&mut bytes)?;
-        bytes[0]
+        let mut byte = 0;
+        $src.read_exact(std::slice::from_mut(&mut byte))?;
+        byte
     }};
     // Use the stack if the length is known at compile-time
     ($src:expr, $n:literal) => {{
