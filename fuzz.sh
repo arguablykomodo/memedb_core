78450feff24de380e375d57d31984821fe198a8c
Komodo <noreply@komodo.ar>
2022-05-01 16:45:17 -0300

    test: actually get correct seed inputs

--- fuzz.sh
+++ fuzz.sh
@@ -2,5 +2,5 @@
 mkdir -p fuzz/corpus/$1
 cargo +nightly fuzz run \
   --strip-dead-code --features memedb_core/$1 $2 fuzz/corpus/$1 -- \
-  -seed_inputs=tests/media/minimal.${1},tests/media/minimal_empty.${1},tests/media/minimal_tagged.${1} \
+  -seed_inputs=tests/media/minimal.${3},tests/media/minimal_empty.${3},tests/media/minimal_tagged.${3} \
   -max_total_time=150
