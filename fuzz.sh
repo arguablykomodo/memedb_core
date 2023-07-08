a413fc7fca3ae39d2674120a6a206a1e83aaad0b
Komodo <noreply@komodo.ar>
2023-07-08 01:19:56 -0300

    ci: update fuzzing tests

--- fuzz.sh
+++ fuzz.sh
@@ -3,4 +3,4 @@ mkdir -p fuzz/corpus/$1
 cargo +nightly fuzz run \
   --strip-dead-code --features memedb_core/$1 $2 fuzz/corpus/$1 -- \
   -seed_inputs=tests/media/minimal.${3},tests/media/minimal_empty.${3},tests/media/minimal_tagged.${3} \
-  -max_total_time=150
+  -max_total_time=120
